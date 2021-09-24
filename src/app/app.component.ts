7014cc259aacd28a897e088339775db4be12859e
WagnerFroes <lucaswagnerp@gmail.com>
2021-09-23 21:09:20 -0300

    2 commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,16 @@ import { Component } from '@angular/core';
 export class AppComponent {
   nome = 'Jose'
   idade = 20
+
+  adicionar(){
+    console.log("Adicionando...")
+  }
+
+  alterarNome(evento){
+    console.log(evento.target.value)
+    this.nome = evento.target.value
+  }
+
   lancarDado(){
     return Math.floor((Math.random() * 6)) + 1
   }
